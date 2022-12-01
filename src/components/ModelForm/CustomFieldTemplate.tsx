05f061dec48d3439c65c5eb7a055280b62a2c0ef
Troy DeSpain <despaintroy@gmail.com>
2022-11-30 20:54:52 -0700

    Custom templates for form

---
+++ src/components/ModelForm/CustomFieldTemplate.tsx
@@ -0,0 +1,26 @@
+import {Templates} from "@rjsf/mui"
+import {FieldTemplateProps} from "@rjsf/utils"
+import React, {ReactElement} from "react"
+import FileField from "./CustomFormatFields/FileField"
+import {ReferenceField} from "./CustomFormatFields/ReferenceField"
+
+export function CustomFieldTemplate(props: FieldTemplateProps): ReactElement {
+
+
+  console.log({
+    label: props.label,
+    format: props.schema.format,
+    references: props.schema.references,
+    props
+  })
+
+  if (props.schema.format === "file") {
+    return <FileField {...props} />
+  }
+
+  if (props.schema.references) {
+    return <ReferenceField {...props} />
+  }
+
+  return <Templates.FieldTemplate {...props} />
+}
